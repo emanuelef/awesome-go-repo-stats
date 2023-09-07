09f9f4f0cc2cc629312a849e65a231b3e0c2a845
Emanuele Fumagalli <emanuelef@gmail.com>
2023-09-07 12:20:21 +0100

    fix GH page base

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cncf-repos-stats/',
+  base: '/awesome-go-repo-stats/',
   plugins: [react()],
 })
