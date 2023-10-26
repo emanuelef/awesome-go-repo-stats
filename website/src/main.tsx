2165f3828aff87e44bc01b8eb0448bed32e62772
Emanuele Fumagalli <emanuelef@gmail.com>
2023-10-26 14:09:02 +0100

    Added GitHub corner

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -4,10 +4,15 @@ import "./index.css";
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 import { HashRouter } from "react-router-dom";
+import GitHubCorners from "@uiw/react-github-corners";
 root.render(
   <React.StrictMode>
     <div>
       <HashRouter>
+        <GitHubCorners
+          position="right"
+          href="https://github.com/emanuelef/awesome-go-repo-stats"
+        />
         <App />
       </HashRouter>
     </div>
