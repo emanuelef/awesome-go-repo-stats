05f8a47346980dc59272accdd9bef4a89927c3b1
Emanuele Fumagalli <emanuelef@gmail.com>
2023-11-04 13:36:35 +0000

    Add star action

--- website/src/Header.jsx
+++ website/src/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Linkweb from "@mui/material/Link";
+import GitHubButton from "react-github-btn";
 
 const csvURL =
   "https://raw.githubusercontent.com/emanuelef/awesome-go-repo-stats/main/analysis-latest.csv";
@@ -15,12 +16,27 @@ function Header({ lastUpdate }) {
     height: "30px",
   };
 
+  const githubButtonStyle = {
+    marginLeft: "auto", // Push GitHub button to the right
+  };
+
   return (
     <div className="header" style={headerStyle}>
       <p>Last Update: {lastUpdate}</p>
       <Linkweb href={csvURL} download>
         Link to CSV
       </Linkweb>
+      <div style={githubButtonStyle}>
+        <GitHubButton
+          href="https://github.com/emanuelef/awesome-go-repo-stats"
+          data-color-scheme="no-preference: dark; light: dark_dimmed; dark: dark_high_contrast;"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star emanuelef/awesome-go-repo-stats on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
     </div>
   );
 }
